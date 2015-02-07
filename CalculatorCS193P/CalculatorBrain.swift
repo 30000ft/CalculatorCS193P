e19da66caca7e9dacb6e74694e6237bff0e0ade1
30000ft <188370985@qq.com>
2015-02-07 22:25:04 +0800

    Implement With MVC

---
+++ CalculatorCS193P/CalculatorBrain.swift
@@ -0,0 +1,54 @@
+//
+//  CalculatorBrain.swift
+//  CalculatorCS193P
+//
+//  Created by Q on 15/2/5.
+//  Copyright (c) 2015年 ywr. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorBrain
+{
+    private enum Op{
+        case Operand(Double)
+        case UnaryOperation(String, Double -> Double)
+        case BinaryOperation(String, (Double, Double) -> Double)
+    }
+    
+    private var opStack = [Op]()
+    
+    private var knownOps = [String:Op]()
+    
+    init() {
+        knownOps["×"] = Op.BinaryOperation("×", *)
+        knownOps["÷"] = Op.BinaryOperation("÷") { $1 / $0 }
+        knownOps["+"] = Op.BinaryOperation("+", +)
+        knownOps["−"] = Op.BinaryOperation("−") { $1 - $0 }
+        knownOps["√"] = Op.UnaryOperation("√", sqrt)
+        
+    }
+    
+    func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op]) {
+        if !ops.isEmpty {
+            var remainingOps = ops
+            let op = remainingOps.removeLast()
+            result
+        }
+        return (nil, ops)
+    }
+    
+    func evaluate() -> Double? {
+        
+    }
+    
+    func pushOperan(operand: Double) {
+        opStack.append(Op.Operand(operand))
+    }
+    
+    func performOperation(symbol: String) {
+        if let operation = knownOps[symbol] {
+            opStack.append(operation)
+        }
+    }
+}
